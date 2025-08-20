9d9d20caba6c02f71ef2767e318cbe3de7ef0dfd
Renektob <renektob@student.21-school.ru>
2025-08-20 21:04:58 +0300

    finish job for developer 4

--- src/tests/test_s21_bit.c
+++ src/tests/test_s21_bit.c
@@ -1,62 +1,140 @@
 #include <check.h>
 #include "../s21_decimal.h"
 
-START_TEST(test_s21_get_bit_basic) {
-    s21_decimal value = {{0}};
-    value.bits[0] = 1; // 00000001
-    
-    ck_assert_int_eq(1, s21_get_bit(value, 0)); // bit 0 should be 1
-    ck_assert_int_eq(0, s21_get_bit(value, 1)); // bit 1 should be 0
-    ck_assert_int_eq(0, s21_get_bit(value, 31)); // bit 31 should be 0
+// 1. Тесты s21_get_bit
+START_TEST(test_s21_get_bit_0) {
+    s21_decimal value = {{1, 0, 0, 0}};
+    ck_assert_int_eq(1, s21_get_bit(value, 0));
 }
 END_TEST
 
-START_TEST(test_s21_set_bit_basic) {
+START_TEST(test_s21_get_bit_1) {
+    s21_decimal value = {{1, 0, 0, 0}};
+    ck_assert_int_eq(0, s21_get_bit(value, 1));
+}
+END_TEST
+
+START_TEST(test_s21_get_bit_31) {
+    s21_decimal value = {{1, 0, 0, 0}};
+    ck_assert_int_eq(0, s21_get_bit(value, 31));
+}
+END_TEST
+
+START_TEST(test_s21_get_bit_first_bit) {
+    s21_decimal value = {{0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF}};
+    ck_assert_int_eq(1, s21_get_bit(value, 0));
+}
+END_TEST
+
+START_TEST(test_s21_get_bit_last_bit_first_word) {
+    s21_decimal value = {{0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF}};
+    ck_assert_int_eq(1, s21_get_bit(value, 31));
+}
+END_TEST
+
+START_TEST(test_s21_get_bit_first_bit_second_word) {
+    s21_decimal value = {{0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF}};
+    ck_assert_int_eq(1, s21_get_bit(value, 32));
+}
+END_TEST
+
+START_TEST(test_s21_get_bit_last_bit_overall) {
+    s21_decimal value = {{0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF}};
+    ck_assert_int_eq(1, s21_get_bit(value, 127));
+}
+END_TEST
+
+START_TEST(test_s21_get_bit_out_of_bounds_negative) {
+    s21_decimal value = {{0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF}};
+    ck_assert_int_eq(0, s21_get_bit(value, -1));
+}
+END_TEST
+
+START_TEST(test_s21_get_bit_out_of_bounds_too_large) {
+    s21_decimal value = {{0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF}};
+    ck_assert_int_eq(0, s21_get_bit(value, 128));
+}
+END_TEST
+
+// 2. Тесты s21_set_bit
+START_TEST(test_s21_set_bit_0_to_1) {
     s21_decimal value = {{0}};
-    
     s21_set_bit(&value, 0, 1);
     ck_assert_int_eq(1, s21_get_bit(value, 0));
-    
+}
+END_TEST
+
+START_TEST(test_s21_set_bit_31_to_1) {
+    s21_decimal value = {{0}};
     s21_set_bit(&value, 31, 1);
     ck_assert_int_eq(1, s21_get_bit(value, 31));
-    
+}
+END_TEST
+
+START_TEST(test_s21_set_bit_0_to_0) {
+    s21_decimal value = {{0}};
+    s21_set_bit(&value, 0, 1);
     s21_set_bit(&value, 0, 0);
     ck_assert_int_eq(0, s21_get_bit(value, 0));
 }
 END_TEST
 
-START_TEST(test_s21_get_bit_edge_cases) {
-    s21_decimal value = {{0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF}};
-    
-    ck_assert_int_eq(1, s21_get_bit(value, 0));   // first bit
-    ck_assert_int_eq(1, s21_get_bit(value, 31));  // last bit of first word
-    ck_assert_int_eq(1, s21_get_bit(value, 32));  // first bit of second word
-    ck_assert_int_eq(1, s21_get_bit(value, 127)); // last bit overall
-    
-    // Test out of bounds
-    ck_assert_int_eq(0, s21_get_bit(value, -1));
-    ck_assert_int_eq(0, s21_get_bit(value, 128));
+START_TEST(test_s21_set_bit_out_of_bounds_negative) {
+    s21_decimal value = {{0}};
+    s21_set_bit(&value, -1, 1);
+    // Должно отработать без падения
+    ck_assert_int_eq(1, 1); // Просто проверяем что не упало
 }
 END_TEST
 
-START_TEST(test_s21_set_bit_edge_cases) {
+START_TEST(test_s21_set_bit_out_of_bounds_too_large) {
     s21_decimal value = {{0}};
-    
-    // Test out of bounds - should not crash
-    s21_set_bit(&value, -1, 1);
     s21_set_bit(&value, 128, 1);
-    
-    // Test setting all bits in first word
-    for (int i = 0; i < 32; i++) {
-        s21_set_bit(&value, i, 1);
-        ck_assert_int_eq(1, s21_get_bit(value, i));
-    }
-    
-    // Test clearing all bits
-    for (int i = 0; i < 32; i++) {
-        s21_set_bit(&value, i, 0);
-        ck_assert_int_eq(0, s21_get_bit(value, i));
-    }
+    // Должно отработать без падения
+    ck_assert_int_eq(1, 1); // Просто проверяем что не упало
+}
+END_TEST
+
+// 3. Тесты циклов (нужно разделить)
+START_TEST(test_s21_set_bit_all_bits_0) {
+    s21_decimal value = {{0}};
+    s21_set_bit(&value, 0, 1);
+    ck_assert_int_eq(1, s21_get_bit(value, 0));
+}
+END_TEST
+
+START_TEST(test_s21_set_bit_all_bits_15) {
+    s21_decimal value = {{0}};
+    s21_set_bit(&value, 15, 1);
+    ck_assert_int_eq(1, s21_get_bit(value, 15));
+}
+END_TEST
+
+START_TEST(test_s21_set_bit_all_bits_31) {
+    s21_decimal value = {{0}};
+    s21_set_bit(&value, 31, 1);
+    ck_assert_int_eq(1, s21_get_bit(value, 31));
+}
+END_TEST
+
+START_TEST(test_s21_clear_bit_all_bits_0) {
+    s21_decimal value = {{0xFFFFFFFF, 0, 0, 0}};
+    s21_set_bit(&value, 0, 0);
+    ck_assert_int_eq(0, s21_get_bit(value, 0));
+}
+END_TEST
+
+START_TEST(test_s21_clear_bit_all_bits_15) {
+    s21_decimal value = {{0xFFFFFFFF, 0, 0, 0}};
+    s21_set_bit(&value, 15, 0);
+    ck_assert_int_eq(0, s21_get_bit(value, 15));
+}
+END_TEST
+
+START_TEST(test_s21_clear_bit_all_bits_31) {
+    s21_decimal value = {{0xFFFFFFFF, 0, 0, 0}};
+    s21_set_bit(&value, 31, 0);
+    ck_assert_int_eq(0, s21_get_bit(value, 31));
 }
 END_TEST
 
@@ -64,10 +142,31 @@ Suite* s21_bit_suite(void) {
     Suite* s = suite_create("s21_bit");
     TCase* tc = tcase_create("core");
     
-    tcase_add_test(tc, test_s21_get_bit_basic);
-    tcase_add_test(tc, test_s21_set_bit_basic);
-    tcase_add_test(tc, test_s21_get_bit_edge_cases);
-    tcase_add_test(tc, test_s21_set_bit_edge_cases);
+    // s21_get_bit tests
+    tcase_add_test(tc, test_s21_get_bit_0);
+    tcase_add_test(tc, test_s21_get_bit_1);
+    tcase_add_test(tc, test_s21_get_bit_31);
+    tcase_add_test(tc, test_s21_get_bit_first_bit);
+    tcase_add_test(tc, test_s21_get_bit_last_bit_first_word);
+    tcase_add_test(tc, test_s21_get_bit_first_bit_second_word);
+    tcase_add_test(tc, test_s21_get_bit_last_bit_overall);
+    tcase_add_test(tc, test_s21_get_bit_out_of_bounds_negative);
+    tcase_add_test(tc, test_s21_get_bit_out_of_bounds_too_large);
+    
+    // s21_set_bit tests
+    tcase_add_test(tc, test_s21_set_bit_0_to_1);
+    tcase_add_test(tc, test_s21_set_bit_31_to_1);
+    tcase_add_test(tc, test_s21_set_bit_0_to_0);
+    tcase_add_test(tc, test_s21_set_bit_out_of_bounds_negative);
+    tcase_add_test(tc, test_s21_set_bit_out_of_bounds_too_large);
+    
+    // Тесты циклов (выборочные проверки вместо полных циклов)
+    tcase_add_test(tc, test_s21_set_bit_all_bits_0);
+    tcase_add_test(tc, test_s21_set_bit_all_bits_15);
+    tcase_add_test(tc, test_s21_set_bit_all_bits_31);
+    tcase_add_test(tc, test_s21_clear_bit_all_bits_0);
+    tcase_add_test(tc, test_s21_clear_bit_all_bits_15);
+    tcase_add_test(tc, test_s21_clear_bit_all_bits_31);
     
     suite_add_tcase(s, tc);
     return s;
